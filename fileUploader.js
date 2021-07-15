0b238c707cfd4037d32e7478d45e8a68a892dc8a
rushilkoppaka-freezonlabs <83945787+rushilkoppaka-freezonlabs@users.noreply.github.com>
2021-07-15 15:48:02 +0530

    Update fileUploader.js

--- fileUploader.js
+++ fileUploader.js
@@ -1,37 +1,33 @@
 '''javascript
 
-var Minio = require('minio')
+//json is suppose stored in clientlist
+//files are stored in doc
+
+function minioStorage(clientlist,doc){
+    var Minio = require('minio')
 
 // Instantiate the minio client with the endpoint
 // and access keys as shown below.
-var minioClient = new Minio.Client({
-    endPoint: 'play.min.io',
-    port: 9000,
-    useSSL: true,
-    accessKey: 'Q3AM3UQ867SPQQA43P2F',
-    secretKey: 'zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG'
-});
-
-// File that needs to be uploaded.
-var file = '/tmp/photos-europe.tar'
-
-// Make a bucket called europetrip.
-minioClient.makeBucket('europetrip', 'us-east-1', function(err) {
-    if (err) return console.log(err)
-
-    console.log('Bucket created successfully in "us-east-1".')
-
-    var metaData = {
-        'Content-Type': 'application/octet-stream',
-        'X-Amz-Meta-Testing': 1234,
-        'example': 5678
-    }
-    // Using fPutObject API upload your file to the bucket europetrip.
-    minioClient.fPutObject('europetrip', 'photos-europe.tar', file, metaData, function(err, etag) {
-      if (err) return console.log(err)
-      console.log('File uploaded successfully.')
+    var minioClient = new Minio.Client({
+        endPoint: 'play.min.io',
+        port: 9000,
+        useSSL: true,
+        accessKey: 'Q3AM3UQ867SPQQA43P2F',
+        secretKey: 'zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG'
     });
-});
+
+    for (let i=0;i<=10;i++){
+            found = minioClient.bucket_exists( clientlist[i].username)
+            if (found==False):
+                minioClient.make_bucket(clientlist[i].username)
+            else:
+                console.log('bucket already exists')
+            // here we upload the data in form of btyearray 
+            //and the str parameters must in lowercase
+            result = minioClient.fPutObject( clientlist[i].username, clientlist[i].filename,
+                                       doc[i].file, -1, part_size=10 * 1024 * 1024)
+            console.log('File uploaded successfully.')
+}
 
 
 
