031772763f51333a75d3a1f67083bcb93bbb21f1
rushilkoppaka-freezonlabs <83945787+rushilkoppaka-freezonlabs@users.noreply.github.com>
2021-07-16 17:12:57 +0530

    Update fileExtract.js

--- fileExtract.js
+++ fileExtract.js
@@ -32,3 +32,31 @@ function minioExtract(username,filename,path){
 
 
 '''
+'''python
+
+
+from minio import Minio
+from minio.error import S3Error
+import io
+
+def listToString(s):
+    str1 = ""
+    for ele in s:
+        str1 += ele
+    return str1
+
+def Extract(inbox, doc,path):
+    client = Minio(
+        "play.min.io",
+        access_key="Q3AM3UQ867SPQQA43P2F",
+        secret_key="zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG",
+    )
+
+    for i in range(len(inbox)):
+        found = client.bucket_exists(inbox["username"][i].lower())
+        if not found:
+            print('doesnt exist')
+        else:
+            result = client.fget_object(inbox["username"][i].lower(), listToString(inbox['file name'][i]).lower(),path)
+            return result
+
